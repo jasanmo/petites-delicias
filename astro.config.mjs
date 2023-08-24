bed63ee24a0f93533cc4fff7d5ba9bebe1609b9f
Javier Sánchez <javi.sm92@gmail.com>
2023-08-24 20:31:08 +0200

    chore: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
-	integrations: [tailwind()],
+	site: "https://petitesdelicias.com",
+	integrations: [tailwind(), sitemap()],
 	vite: {
 		ssr: {
 			noExternal: ["astro-seo-schema"],
