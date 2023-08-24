01143054216df618bb5cd38041894a8138f5bb31
Javier Sánchez <javi.sm92@gmail.com>
2023-08-24 20:16:46 +0200

    chore:

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly CF_BASE_URL: string;
+  readonly CONTENTFU_BASE_IMAGE_URL: string;
+  readonly CONTENTFUL_DELIVERY_TOKEN: string;
+  readonly CONTENTFUL_PREVIEW_TOKEN: string;
+  readonly CONTENTFU_SPACE_ID: string;
+  readonly CONTENTFU_ENVIRONMENT: string;
+}
