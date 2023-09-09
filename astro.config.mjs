7005d3911edd8ed0899de6e9879346cb8075428e
Javier Sánchez <javi.sm92@gmail.com>
2023-09-09 19:17:33 +0200

    fix:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://petitesdelicias.com",
 	integrations: [tailwind(), sitemap()],
+	redirects: {
+		"/tag/mona/": "/",
+		"/tag/crepes/": "/",
+		"/tag/almuerzo/": "/",
+		"/tag/verduras/": "/",
+		"/tag/flan/": "/",
+		"/tag/guarnicion/": "/",
+		"/tag/meriendas/": "/",
+		"/page/2/": "/",
+	},
 });
